47b4be6b037c20462298fdbb5f6f2cab68257f62
Evan Watterud <cod.videos@hotmail.com>
2016-04-08 18:14:27 -0400

    initial commit

---
+++ solutions.js
@@ -0,0 +1,49 @@
+// Exercise: Find the top navbar by query for the element type, which is <nav>.
+$('nav');
+
+// Exercise: Find the sidebar on the right by using it's id.
+$('#sidebar-right');
+
+// Exercise: Find the "Pages" and "Groups" sections of the sidebar by using their class.
+$('.pages');
+$('.groups');
+
+// Exercise: Find all of the comments on the page.
+$('.comments > div');
+
+// Exercise: Find the first comment on the page.
+$('.comments > div').first();
+
+// Exercise: Find the last comment on the page.
+$('.comments > div').last();
+
+// Exercise: Find the fourth comment on the page.
+$($('.comments > div')[3]);
+
+// Exercise: Find one of the ads in the sidebar and use .hide() to hide it.
+$('.ad-slot:nth-child(1)').hide();
+
+// Exercise: Use .show() to make the ad that you hid in the previous step visible again.
+$('.ad-slot:nth-child(1)').show();
+
+// Exercise: Use .toggle() to toggle the display of the nav bar at the top of the page.
+$('nav').toggle();
+
+// Exercise: Use .attr() to change src attribute of one of the ads in the sidebar. Here's an image source if you need one: http://placebear.com/200/300.
+var $image = $('.ad-slot img').first();
+$image.attr('src', 'http://placebear.com/200/300');
+
+// Exercise: Find Sam's post and change it's text to something incredible.
+$($('.post p')[8]).text('Objective C is trash, all C is trash.');
+
+// Exercise: Find the first post and use .addClass() to add the .post-liked class to it.
+var $firstPost = $('.post:first');
+$firstPost.addClass('post-liked');
+
+// Exercise: Find the second post and use .removeClass() to remove the .post-liked class.
+var $secondPost = $('.post:nth-child(2)');
+$secondPost.removeClass('post-liked');
+
+// Exercise: Find any post and use .toggleClass() to toggle the .post-liked class.
+var $secondPost = $('.post:nth-child(2)');
+$secondPost.toggleClass('post-liked');
